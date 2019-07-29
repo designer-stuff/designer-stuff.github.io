899f1f93f226b2e0493b3681ea2a67fb4b92c383
designer-stuff <sk990005@outlook.com>
2019-07-29 19:23:53 +0530

    Second commit
    Serenity Modified

--- script.js
+++ script.js
@@ -2,7 +2,40 @@
 AOS.init();
 
 // Smooth Scrolling initialization
-var scroll = new SmoothScroll('a[href*="#"]', {
+let scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   offset: 150
+});
+
+// Maps
+function initMap() {
+  let location = {
+    lat: 15.5202268,
+    lng: 73.767442
+  };
+
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: location
+  });
+
+  let marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+
+// submit comment
+let submit = document.getElementById("submit");
+let name = document.getElementById("name");
+let comment = document.getElementById("comment");
+
+submit.addEventListener("click", e => {
+  if (name.value.length === 0 || comment.value.length === 0)
+    alert("Please fill the required fields");
+  else
+    alert("Feedback submitted successfully. Thanks for your valuable comment");
+
+  name.value = "";
+  comment.value = "";
 });
